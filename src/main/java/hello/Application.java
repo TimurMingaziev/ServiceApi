210aeeabdeae6838e5b4970139ea87c7a4516faa
TimurMingaziev <tminraz@gmail.com>
2016-03-13 12:02:08 +0200

    hib + 1 method getListMq

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,9 +3,15 @@ package hello;
 /**
  * Created by Switch on 03.03.2016.
  */
+import hello.DAO.TypeMqDAO;
+import hello.impl.TypeMqImpl;
+import hello.model.TypeMq;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.util.List;
 
 @ComponentScan
 @EnableAutoConfiguration
@@ -13,5 +19,18 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+//        TypeMqDAO typeMq = new TypeMqImpl();
+//        listTypeMq(typeMq.getInfoAboutAll());
+
     }
+//    private static void listTypeMq(List<TypeMq> typesMq) {
+//        System.out.println("");
+//        System.out.println("Listing contacts without details:");
+//        for (TypeMq typeMq : typesMq) {
+//            System.out.println(typeMq);
+//            System.out.println();
+//        }
+//    }
+
 }
--- src/main/java/hello/CountryEndpoint.java
+++ src/main/java/hello/CountryEndpoint.java
@@ -4,15 +4,18 @@ package hello;
  * Created by Switch on 03.03.2016.
  */
 
-import io.spring.guides.gs_producing_web_service.GetListCountry;
+import hello.DAO.TypeMqDAO;
+import hello.impl.TypeMqImpl;
+import hello.model.*;
+import io.spring.guides.gs_producing_web_service.*;
+import io.spring.guides.gs_producing_web_service.TypeMq;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
-import io.spring.guides.gs_producing_web_service.GetCountryRequest;
-import io.spring.guides.gs_producing_web_service.GetCountryResponse;
+import java.util.List;
 
 @Endpoint //регистрирует класс Spring WS как потенциальный кандидат для обработки входящих SOAP сообщений
 public class CountryEndpoint {
@@ -26,7 +29,7 @@ public class CountryEndpoint {
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest") //используется Spring WS для выбора метода
-                                                                            // обработчика на основе namespace и localPart сообщения
+                                                               // обработчика на основе namespace и localPart сообщения
     @ResponsePayload //указывает на то, что входящее сообщение будет сопоставлено параметру request метода
     public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request)  //создает соответствующее значение возвращаемому
                                                                                     // значению полезной части ответа.
@@ -37,5 +40,14 @@ public class CountryEndpoint {
         return response;
     }
 
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getTypeMQListRequest")
+    @ResponsePayload
+    public GetTypeMQListResponce getTypeMQList(@RequestPayload GetTypeMQListRequest request){
+        GetTypeMQListResponce responce = new GetTypeMQListResponce();
+        TypeMqDAO typeMq = new TypeMqImpl();
+
+        responce.getListTypeMq().addAll(typeMq.getInfoAboutAll(request.getToken(),request.getMinMqId(),request.getMaxMqId()));
+        return responce;
+    }
 
 }
